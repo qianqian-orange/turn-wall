1e2f44afc8583ab67c4c0228ee053ae19f5813fe
wunjunjia <2562547453@qq.com>
2019-10-14 12:10:43 +0800

    提交代码

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+app.use('/', (req, res) => {
+    axios.get('http://www.pinterest.com/')
+        .then((result) => {
+            console.log(result.data);
+            res.send('ok');
+        });
+});
+
+app.listen(3000, () => {
+    console.log('server started at port 3000');
+});
