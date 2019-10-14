2f118bc74dae0692b530d434a14bf8b12b445000
wunjunjia <2562547453@qq.com>
2019-10-14 18:59:21 +0800

    update

--- src/index.js
+++ src/index.js
@@ -14,14 +14,14 @@ router.get('/index', (req, res) => {
 });
 
 // js
-router.get('/warm', (req, res) => {
-    axios.get('https://i.pinimg.com/assets/js/warm.js?_=1571036246321')
-        .then((result) => {
-            res.send(result.data);
-        })
-});
-
-app.use(router);
+// router.get('/warm', (req, res) => {
+//     axios.get('https://i.pinimg.com/assets/js/warm.js?_=1571036246321')
+//         .then((result) => {
+//             res.send(result.data);
+//         })
+// });
+//
+// app.use(router);
 
 // css
 const CSSRouter = express.Router();
