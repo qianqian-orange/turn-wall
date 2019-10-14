4c1eaa9a92fcf83427cdb0d0d2239c7ecfca681d
wunjunjia <2562547453@qq.com>
2019-10-14 14:55:10 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('ok');
+});
+
+app.use(router);
 
 // app.use('/', (req, res) => {
-    axios.get('http://www.pinterest.com/')
-        .then((result) => {
-            console.log(result.data);
-            // res.send('ok');
-        });
+//     axios.get('http://www.pinterest.com/')
+//         .then((result) => {
+//             res.send(result.data);
+//         });
 // });
+
 
 app.listen(3001, () => {
     console.log('server started at port 3001');
